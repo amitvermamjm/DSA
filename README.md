30a1e7eb3a5d598ed0d6ed73cca2caa1afc4d3ad
Amit Verma <amitvermamjm@gmail.com>
2026-01-12 02:51:20 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# DSA topics for Revision
+
+A list of all Data Structure  
+A list of all Algorithms
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div> List of all Data Structure and Algorithms </div>
+    </>
+  )
+}
+
+export default App
