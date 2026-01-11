369453ec73be784bc2cf901209b8aa9126f21fab
Amit Verma <amitvermamjm@gmail.com>
2026-01-12 04:10:36 +0530

    added details in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,68 @@ import './App.css'
 function App() {
   return (
     <>
-      <div> List of all Data Structure and Algorithms </div>
+      <div> <h1>List of all Data Structure and Algorithms</h1>
+        <h2>Data Structures</h2>
+        <ul>
+          <li>Array</li>
+          <li>Linked List</li>
+          <li>Stack</li>
+          <li>Queue</li>
+          <li>Hash Table</li>
+          <li>Tree</li>
+          <li>Graph</li>
+        </ul>
+        <h2>Algorithms</h2>
+        <ul>
+          <li>Sorting Algorithms
+            <ul>
+              <li>Bubble Sort</li>
+              <li>Selection Sort</li>
+              <li>Insertion Sort</li>
+              <li>Merge Sort</li>
+              <li>Quick Sort</li>
+            </ul>
+          </li>
+          <li>Searching Algorithms
+            <ul>
+              <li>Linear Search</li>
+              <li>Binary Search</li>
+            </ul>
+          </li>
+        </ul>
+        <h2>Graph Algorithms</h2>
+        <ul>
+          <li>Depth-First Search (DFS)</li>
+          <li>Breadth-First Search (BFS)</li>
+          <li>Dijkstra's Algorithm</li>
+          <li>A* Search Algorithm</li>
+        </ul>
+        <h2>Dynamic Programming</h2>
+        <ul>
+          <li>Fibonacci Sequence</li>
+          <li>Knapsack Problem</li>
+          <li>Longest Common Subsequence</li>
+        </ul>
+        <h2>Recursion</h2>
+        <ul>
+          <li>Factorial Calculation</li>
+          <li>Fibonacci Sequence</li>
+          <li>Tower of Hanoi</li>
+        </ul>
+        <h2>Miscellaneous Topics</h2>
+        <ul>
+          <li>Big O Notation</li>
+          <li>Greedy Algorithms</li>
+          <li>Backtracking</li>
+        </ul>
+        <h2>Practice Platforms</h2>
+        <ul>
+          <li>LeetCode</li>
+          <li>HackerRank</li>
+          <li>CodeSignal</li>
+          <li>GeeksforGeeks</li>
+        </ul>
+      </div>
     </>
   )
 }
